d99358e5096db42df347aad2abbcede152c54d34
Joel Jose <joeljose420@gmail.com>
2021-11-25 12:42:31 +0530

    update

--- ShutAllInterface/shutRtr1.sh
+++ ShutAllInterface/shutRtr1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python shutinterfaces.py --testbed-file testbed.yaml --devices dist-rtr01
+python shutinterfaces.py --testbed-file testbed.yaml --devices dist-rtr01 --exclude dist-rtr01:GigabitEthernet1
